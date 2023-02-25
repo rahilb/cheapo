0364a67fcf5cedb36b8adf9aaeb21a2eeb715794
Rahil Bohra <rahil@bohra.co.uk>
2023-02-25 15:25:53 +0000

    TF for bohra.co.uk

--- certbot.tf
+++ certbot.tf
@@ -21,7 +21,7 @@ resource "null_resource" "certbot_install" {
   provisioner "remote-exec" {
     inline = [
       "/tmp/wait-for-dns.sh '${digitalocean_droplet.nginx_server.ipv4_address}'",
-      "certbot --nginx -d bohra.co.uk -d www.bohra.co.uk --non-interactive --agree-tos -m ${var.cert_contact} --debug-challenges",
+      "certbot --nginx -d ${var.domain_name} -d www.${var.domain_name} --non-interactive --agree-tos -m ${var.cert_contact} --debug-challenges",
     ]
   }
 
